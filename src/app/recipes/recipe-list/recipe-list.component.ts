0b0e5cb96b73ce5bdd65a28b3fb5060fa98c99d6
gruyuri <gruyuri@yandex.ru>
2019-12-11 23:12:27 +0100

    Event binding between child components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  @Output() recipeWasSelected: EventEmitter<Recipe> = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
     new Recipe('A Test 1 Recipe', 'This is a simple Pizza',
@@ -20,4 +22,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
