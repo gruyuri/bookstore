6413a9d6a51484f3a4df3424f7b370d4028051bb
gruyuri <33548641+gruyuri@users.noreply.github.com>
2019-12-13 09:07:22 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -12,6 +12,14 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+    
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
